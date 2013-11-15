c7cbea138d4c3687c0813cb31cb7ec9ccef362e8
tao2.guo <tao2.guo@symbio.com>
2013-11-15 14:07:50 +0800

    fte-543

--- public/js/example.js
+++ public/js/example.js
@@ -5,8 +5,14 @@ $.fn.extend({
 			url: url,
 			dataType: 'json',
 			success: function(data){
-				alert(data);
-				$('.example-hide-imgbox').html(data.html);
+				$('.img-wrapper').html(data.html);
+				if($('.example-hide-imgbox').has('img')){
+					
+					$('.example-hide-imgbox').css({
+						'left': ($(window).width() - 800)/2 + 'px',
+						'top' : ($(window).height() - 625)/2 + 'px'
+					}).fadeIn();
+				}
 			}
 		})
 	}
@@ -15,5 +21,8 @@ $(document).ready(function(){
 	$('.example-ajax-request').bind('click', function(event){
 		event.preventDefault();
 		$.fn.exampleAjaxRequest.call($(this));
+	});
+	$('.example-hide-imgbox span').bind('click', function(){
+		$(this).parent().fadeOut();
 	})
 });
